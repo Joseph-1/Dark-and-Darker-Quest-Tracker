1f29d0cca3fc05e40ee28fef36f972534c9cc4f8
Joseph <anthony.joseph67150@gmail.com>
2025-06-18 19:08:44 +0200

    feat: modify routing and path for link in Quest views

--- src/Controller/MerchantController.php
+++ src/Controller/MerchantController.php
@@ -11,10 +11,10 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-#[Route('/merchant')]
+#[Route('/merchant', name: 'merchant_')]
 final class MerchantController extends AbstractController
 {
-    #[Route(name: 'app_merchant_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(MerchantRepository $merchantRepository): Response
     {
         return $this->render('merchant/index.html.twig', [
@@ -22,7 +22,7 @@ final class MerchantController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_merchant_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $merchant = new Merchant();
@@ -33,7 +33,7 @@ final class MerchantController extends AbstractController
             $entityManager->persist($merchant);
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_merchant_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('merchant_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('merchant/new.html.twig', [
@@ -42,7 +42,7 @@ final class MerchantController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_merchant_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Merchant $merchant): Response
     {
         return $this->render('merchant/show.html.twig', [
@@ -50,7 +50,7 @@ final class MerchantController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_merchant_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Merchant $merchant, EntityManagerInterface $entityManager): Response
     {
         $form = $this->createForm(MerchantForm::class, $merchant);
@@ -59,7 +59,7 @@ final class MerchantController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $entityManager->flush();
 
-            return $this->redirectToRoute('app_merchant_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('merchant_index', [], Response::HTTP_SEE_OTHER);
         }
 
         return $this->render('merchant/edit.html.twig', [
@@ -68,7 +68,7 @@ final class MerchantController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_merchant_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Merchant $merchant, EntityManagerInterface $entityManager): Response
     {
         if ($this->isCsrfTokenValid('delete'.$merchant->getId(), $request->getPayload()->getString('_token'))) {
@@ -76,6 +76,6 @@ final class MerchantController extends AbstractController
             $entityManager->flush();
         }
 
-        return $this->redirectToRoute('app_merchant_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('merchant_index', [], Response::HTTP_SEE_OTHER);
     }
 }
