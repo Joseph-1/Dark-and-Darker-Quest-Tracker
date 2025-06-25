0e57cc1757e6be1a0f6c7f57b03746d9b70f61ce
Joseph <anthony.joseph67150@gmail.com>
2025-06-26 00:25:09 +0200

    feat: add some Validation to User entity

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -7,6 +7,7 @@ use Doctrine\ORM\Mapping as ORM;
 use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
 use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 #[ORM\UniqueConstraint(name: 'UNIQ_IDENTIFIER_EMAIL', fields: ['email'])]
@@ -18,7 +19,13 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
     #[ORM\Column]
     private ?int $id = null;
 
-    #[ORM\Column(length: 180)]
+    #[ORM\Column(length: 180, unique: true)]
+    #[Assert\NotBlank(message: 'You must enter an email')]
+    #[Assert\Email(message: 'You must enter a valid email')]
+    #[Assert\Length(
+        max: 180,
+        maxMessage: 'Your email {{ value }} do not exceed {{ limit }} characters'
+    )]
     private ?string $email = null;
 
     /**
@@ -31,12 +38,25 @@ class User implements UserInterface, PasswordAuthenticatedUserInterface
      * @var string The hashed password
      */
     #[ORM\Column]
+    #[Assert\NotBlank(message: 'You must enter a password')]
+    #[Assert\Length(
+        min: 6,
+        max: 4096, // Symfony's limit to dodge DoS attack
+        minMessage: 'Your password should be at least {{ limit }} characters',
+    )]
     private ?string $password = null;
 
     #[ORM\Column]
     private ?bool $isVerified = false;
 
     #[ORM\Column(length: 30)]
+    #[Assert\NotBlank(message: 'You must provide a nickname')]
+    #[Assert\Length(
+        min: 5,
+        max: 30,
+        minMessage: 'Your nickname must be at least {{ limit }} characters',
+        maxMessage: 'Your nickname cannot be longer than {{ limit }} characters'
+    )]
     private ?string $nickname = null;
 
     public function getId(): ?int
